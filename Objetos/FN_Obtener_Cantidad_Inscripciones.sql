bdb4d77bcf2127f2c53e527196ac173656ddc9e8
Felipe Torres <filip-2@hotmail.com>
2024-12-28 20:08:16 -0500

    Agregando actualizacion de entrega final

--- Objetos/FN_Obtener_Cantidad_Inscripciones.sql
+++ Objetos/FN_Obtener_Cantidad_Inscripciones.sql
@@ -2,24 +2,20 @@ USE CorcheaAcademyDB;
 
 DELIMITER $$
 
-DROP FUNCTION IF EXISTS Calcular_Total_Pagos_Usuario$$
+DROP FUNCTION IF EXISTS Obtener_Cantidad_Inscripciones$$
 
-CREATE FUNCTION Calcular_Total_Pagos_Usuario(id_usuario_input INT)
-RETURNS DECIMAL(10, 2)
+CREATE FUNCTION Obtener_Cantidad_Inscripciones(id_usuario_input INT)
+RETURNS INT
 DETERMINISTIC
 BEGIN
-    DECLARE total DECIMAL(10, 2);
-    
-    -- Se suma el monto de las inscripciones del usuario
-    SELECT SUM(p.monto) INTO total
-    FROM Pagos p
-    JOIN Inscripciones i ON p.id_inscripcion = i.id_inscripcion
-    WHERE i.id_usuario = id_usuario_input;
-
-    RETURN COALESCE(total, 0.00);
+    DECLARE cantidad INT;
+    SELECT COUNT(*) INTO cantidad
+    FROM Inscripciones
+    WHERE id_usuario = id_usuario_input;
+    RETURN cantidad;
 END$$
 
 DELIMITER ;
 
 -- Ejemplo de uso:
-SELECT Calcular_Total_Pagos_Usuario(1) AS Total_Pagado;
+SELECT Obtener_Cantidad_Inscripciones(1) AS Total_Inscripciones;
